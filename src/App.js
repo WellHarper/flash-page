7762c624aa15e4c9736f6aa2655ebe2146e8f99e
Wellerson <wellersonramos360@gmail.com>
2020-01-21 12:33:00 -0300

    adiciona linha e coluna

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Row, Col} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <h1>f</h1>
+      <Row>
+        <Col md={{span: 2, offset: 10}}>
+            <span>Foto</span>
+            <span>Nome</span>
+        </Col>
+      </Row>
     </div>
   );
 }
