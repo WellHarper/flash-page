2a2793bfb12185e801a1d8c7b95db0c5afb23038
Wellerson <wellersonramos360@gmail.com>
2020-01-22 12:54:51 -0300

    continua replica

--- src/App.js
+++ src/App.js
@@ -6,11 +6,24 @@ function App() {
   return (
     <div className="App">
       <Row>
-        <Col md={{span: 2, offset: 10}}>
-            <span>Foto</span>
+      <Col md={{span: 2, offset:10}} className="avatar">
+            <span className="foto">FT</span>
             <span>Nome</span>
+      </Col>  
+      </Row>
+      <Row>
+        <Col className="title">
+        <h1>FLASH</h1>  
+        <span>Home/Flash</span>
         </Col>
       </Row>
+      <Row>
+        <Col md={{span: 2}} className="option">OI</Col>
+        <Col md={{span: 2}}className="option">HELLO</Col>
+        <Col md={{span: 2}}className="option">oLA</Col>
+      </Row>
+      
+      
     </div>
   );
 }
